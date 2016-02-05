ed6a7081fbf4c6f30625463095a9e393cbd8e4da
emivo <emil.airta@helsinki.fi>
2016-02-05 09:33:59 +0200

    tehtäviä viikolla 3

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,6 +2,8 @@ class Beer < ActiveRecord::Base
   include RatingAverage
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
+
+  validates :name, allow_blank:  false
   def to_s
     "#{self.name}, #{self.brewery.name}"
   end
